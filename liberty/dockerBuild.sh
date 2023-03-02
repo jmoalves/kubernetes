af6cc48c18bb14289bbadceca17962a9e7a033d0
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2023-03-02 20:36:01 +0000

    Liberty - docker build - parâmetros

--- liberty/dockerBuild.sh
+++ liberty/dockerBuild.sh
@@ -2,21 +2,46 @@
 
 clear
 
-libertyVersion=23.0.0.1
-jdkVersion=17.0.6.0
-
-libertyFlavor=openliberty
-#libertyFlavor=wlp
 jdkFlavor=semeru
-#jdkFlavor=ibm
+jdkVersion=
+libertyFlavor=openliberty
+libertyVersion=
 
-jdkUrl=https://github.com/ibmruntimes/semeru17-binaries/releases/download/jdk-17.0.6%2B10_openj9-0.36.0/ibm-semeru-open-jre_x64_linux_17.0.6_10_openj9-0.36.0.tar.gz
+while getopts "J:j:L:l:" o; do
+    case "${o}" in
+    J) jdkFlavor="${OPTARG}";;
+    j) jdkVersion="${OPTARG}";;
+    L) libertyFlavor="${OPTARG}";;
+    l) libertyVersion="${OPTARG}";;
+    *)
+        echo Invalid options
+        exit 1
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+if [ -z "${jdkVersion}" ]; then
+    echo Inform the jdk version with the '-j' option
+    exit 1
+fi
+
+if [ -z "${libertyVersion}" ]; then
+    echo Inform the liberty version with the '-l' option
+    exit 1
+fi
+
+imageName=liberty:${libertyFlavor}-${libertyVersion}-${jdkFlavor}-${jdkVersion}
+
+echo ==========================================
+echo === Liberty - building image ${imageName}
+echo
 
 docker build \
     --build-arg jdkFlavor=${jdkFlavor} \
     --build-arg jdkVersion=${jdkVersion} \
     --build-arg libertyFlavor=${libertyFlavor} \
     --build-arg libertyVersion=${libertyVersion} \
-    -t jmoalves/liberty:${libertyFlavor}-${libertyVersion}-${jdkFlavor}-${jdkVersion} \
+    -t jmoalves/${imageName} \
     --progress=plain \
     .
